c41a21a0ac4bd47721c91ddfe72835487ad41374
Marvin Borisch <coppnic@e.email>
2024-11-20 23:38:21 +0100

    Update LinksData.js

--- data/LinksData.js
+++ data/LinksData.js
@@ -2,38 +2,24 @@
 const webLinks = [
     // All social profile
     {
+        title: 'Twitch',
+        url: 'https://twitch.tv/dotPixelmonarch',
+        type: 'social',
+        icon: '/twitch.svg',
+        on: true
+    },
+    {
         title: 'Twitter',
-        url: 'https://twitter.com/realvjy',
+        url: 'https://twitter.com/dotPixelmonarch',
         type: 'social',
         icon: '/twitter.svg',
         on: true
     },
     {
-        title: 'Instagram',
-        url: 'https://instagram.com/realvjy',
+        title: 'Blusky',
+        url: 'https://bsky.app/profile/pxl.coppnic.cc',
         type: 'social',
-        icon: '/insta.svg',
-        on: true
-    },
-    {
-        title: 'Dribbble',
-        url: 'https://dribbble.com/realvjy',
-        type: 'social',
-        icon: '/dribbble.svg',
-        on: false
-    },
-    {
-        title: 'Medium',
-        url: 'https://medium.com/@realvjy',
-        type: 'social',
-        icon: '/medium.svg',
-        on: false
-    },
-    {
-        title: 'Github',
-        url: 'https://github.com/realvjy',
-        type: 'social',
-        icon: '/github.svg',
+        icon: '/bsky.svg',
         on: true
     },
     {
@@ -41,69 +27,6 @@ const webLinks = [
         url: 'https://youtube.com/realvjy',
         type: 'social',
         icon: '/youtube.svg',
-        on: false
-    },
-    {
-        title: 'Figma',
-        url: 'https://figma.com/@realvjy',
-        type: 'social',
-        icon: '/figma.svg',
-        on: true
-    },
-    {
-        title: 'Behance',
-        url: 'https://behance.net/realvjy',
-        type: 'social',
-        icon: '/behance.svg',
-        on: false
-    },
-    {
-        title: 'Linkedin',
-        url: 'https://www.linkedin.com/in/realvjy/',
-        type: 'social',
-        icon: '/linkedin.svg',
-        on: false
-    },
-    {
-        title: 'Mastodon',
-        url: 'https://mastodon.online/@realvjy',
-        type: 'social',
-        icon: '/mastodon.svg',
-        on: true
-    },
-    {
-        title: 'Hive Social',
-        url: 'https://www.hivesocial.app/?@realvjy',
-        type: 'social',
-        icon: '/hive.svg',
-        on: false
-    },
-    {
-        title: 'Post',
-        url: 'https://www.post.news/realvjy',
-        type: 'social',
-        icon: '/post.svg',
-        on: false
-    },
-    {
-        title: 'foundation',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/foundation.svg',
-        on: false
-    },
-    {
-        title: 'opensea',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/opensea.svg',
-        on: false
-    },
-    {
-        title: 'new collection',
-        url: 'https://vjy.me',
-        type: 'nft',
-        icon: '/opensea.svg',
         on: false
     },
     // Install section
