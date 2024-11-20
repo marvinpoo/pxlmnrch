bcd47fffd1d0035fe5d4d2ea5095b7c2cb51c5a0
Marvin Borisch <coppnic@e.email>
2024-11-20 23:29:49 +0100

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,21 +1,21 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
-        url: 'https://nxtlnk.vercel.app',
+        name: 'Pixelmonarch',
+        username: 'dotPixelmonarch',
+        url: 'https://twitch.tv/dotPixelmonarch',
         titleImg: true,
         avatar: '/avatar.png',
-        nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        nftAvatar: false,
+        description: 'Just a random gamer that streams his adventures.',
         descShow: true,
-        subdesc: 'Use this template and deploy on vercel for free',
+        subdesc: 'I play rpg, gacha, rythm and strategy. Possible a mix of all of these.',
         subdescShow: true,
-        newProductUrl: 'https://3dicons.co',
+        newProductUrl: 'https://vienna1999.pxlmon.com',
         newProduct: true,
-        author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
-        footerText: 'made by'
+        author: 'coppnic',
+        authorURL: 'https://coppnic.cc',
+        footerText: 'Pixelmonarch is a identity by '
     },
 ];
 
